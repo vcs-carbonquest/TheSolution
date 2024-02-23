bcd1359626e6d4a35684aa3ded8aab3d0f253cb2
Nived Sudhakar <nived.s.2018@gmail.com>
2024-02-22 20:52:55 -0800

    Code Updated
    Electrolysis removed
    Solenoid 2 removed
    Shower Removed
    Sensing for filling container with water and magnesia updated

--- Code/Solution/Fantaseas/XPRIZE Carbon Automation Side A/src/main.cpp
+++ Code/Solution/Fantaseas/XPRIZE Carbon Automation Side A/src/main.cpp
@@ -1,18 +1,84 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Set up I/O Pins
+const int waterPump = 53;
+const int waterLevelSensor = 40;
+const int magnesiaLevelSensor = 44;
+const int sensorSignalMode = 36;
+const int solenoidPrimary = 4;
+//const int electrodialsysZapPin = 10;
+const int solenoidMagnesium = 7;
+const int magnesiumPump = 13;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Setup
+void setup()
+{
+  // Set Pinout
+  Serial.begin(9600);
+  pinMode(waterPump, OUTPUT);
+  pinMode(waterLevelSensor, INPUT);
+  pinMode(magnesiaLevelSensor, INPUT);
+  pinMode(sensorSignalMode, OUTPUT);
+  pinMode(solenoidPrimary, OUTPUT);
+  //pinMode(electrodialsysZapPin, OUTPUT);
+  pinMode(solenoidMagnesium, OUTPUT);
+  pinMode(magnesiumPump, OUTPUT);
+
+  digitalWrite(sensorSignalMode, HIGH);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  // turn on pump
+  digitalWrite(waterPump, HIGH);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  while (digitalRead(waterLevelSensor) != HIGH)
+  {
+    delay(100);
+  }
+
+  digitalWrite(waterPump, LOW);
+
+  delay(1000);
+
+
+
+
+  // Add Milk of Magnesia
+
+  digitalWrite(magnesiumPump, HIGH);
+
+  while (digitalRead(magnesiaLevelSensor) != HIGH)
+  {
+    delay(100);
+  }
+
+  digitalWrite(magnesiumPump, LOW);
+
+
+  // Close Everything
+
+  digitalWrite(solenoidPrimary, LOW);
+
+  /* Electro Dialysis
+
+  digitalWrite(electrodialsysZapPin, HIGH);
+  delay(50400000); // correct number i hope
+  digitalWrite(electrodialsysZapPin, LOW);
+
+  */
+  
+
+  // Drain 
+
+  digitalWrite(solenoidPrimary, HIGH);
+
+  delay(10000); // random number - not accurate
+
+  // close solenoidPrimary
+
+  digitalWrite(solenoidPrimary, LOW);
+
+
+  // End one cycle
 }
